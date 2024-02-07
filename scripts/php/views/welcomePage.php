9f344bf514d8140e13dcfd558266219145e8fcae
KUNMIN WANG <119509864+alterSaika@users.noreply.github.com>
2024-02-07 09:44:00 +0100

    test2

--- scripts/php/views/welcomePage.php
+++ scripts/php/views/welcomePage.php
@@ -25,7 +25,7 @@
         <main>
             <h3>Welcome <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname'] . "!!"; ?></h3>
             <p>First line</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Second line</p>
 
             <!-- A form to logout -->
             <!-- It redirects to the form controller -->
